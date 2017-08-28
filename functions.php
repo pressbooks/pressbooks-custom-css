d7293947aa00af3361cee4d7e29dad2205b1d470
Dac Chartrand <dac.chartrand@gmail.com>
2017-08-28 13:15:47 -0400

    Move code from pressbooks core.

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+
+// -------------------------------------------------------------------------------------------------------------------
+// Check minimum requirements
+// -------------------------------------------------------------------------------------------------------------------
+
+if ( ! function_exists( 'pb_meets_minimum_requirements' ) && ! @include_once( WP_PLUGIN_DIR . '/pressbooks/compatibility.php' ) ) { // @codingStandardsIgnoreLine
+	return add_action( 'admin_notices', function () {
+		echo '<div id="message" class="error fade"><p>' . __( 'Cannot find Pressbooks install.', 'pressbooks-stats' ) . '</p></div>';
+	} );
+} elseif ( ! pb_meets_minimum_requirements() ) {
+	return;
+}
+
+// -------------------------------------------------------------------------------------------------------------------
+// Class autoloader
+// -------------------------------------------------------------------------------------------------------------------
+
+\HM\Autoloader\register_class_path( 'PressbooksCustomCssTheme', __DIR__ . '/inc' );
+
+// -------------------------------------------------------------------------------------------------------------------
+// Requires
+// -------------------------------------------------------------------------------------------------------------------
+
+require( __DIR__ . '/inc/namespace.php' );
+
+// -------------------------------------------------------------------------------------------------------------------
+// Hooks
+// -------------------------------------------------------------------------------------------------------------------
+
+if ( is_admin() ) {
+	add_action( 'init', '\PressbooksCustomCssTheme\form_submit', 50 );
+	add_action( 'admin_menu', '\PressbooksCustomCssTheme\add_menu' );
+	add_action( 'load-post.php', '\PressbooksCustomCssTheme\redirect_css_editor' );
+	add_action( 'wp_ajax_pb_load_css_from', '\PressbooksCustomCssTheme\load_css_from' );
+}
